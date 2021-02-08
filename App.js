cb704329ce7fd66b9919d2c754e15e56a76bcd0b
Cameron Nye <csnye86@gmail.com>
2021-02-08 16:23:06 -0700

    Added state/onPress

--- App.js
+++ App.js
@@ -1,38 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react"
+import { StyleSheet, Text, View, Button } from "react-native"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello, world!</Text>
-      </View>
-      <View style={styles.body}>
-        <Text>Lorem ipsum dolor sit amet.</Text>
-        <Text>Lorem ipsum dolor sit amet.</Text>
-        <Text>Lorem ipsum dolor sit amet.</Text>
-      </View>
-    </View>
-  );
+	const [ name, setName ] = useState("Cam")
+  const [person, setPerson] = useState({
+    name: "Athena",
+    age: 30
+  })
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.boldText}>My name is {name}</Text>
+			<Text>Her name is {person.name} and her age is {person.age}</Text>
+			<View style={styles.buttonContainer}>
+				<Button title="Update Name" onPress={() => {
+          setName("Athena")
+          setPerson({...person, age: 55})
+          }}/>
+			</View>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    backgroundColor: 'red',
-    padding: 20
-  },
-  boldText: {
-    fontWeight: 'bold'
-  },
-  body: {
-    backgroundColor: 'gray',
-    padding: 20
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	buttonContainer: {
+    marginTop: 20
   }
-});
+})
