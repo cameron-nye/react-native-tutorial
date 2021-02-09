7f22c67342073cf266c287ab6bb385b6cf1c17e8
Cameron Nye <csnye86@gmail.com>
2021-02-09 09:57:58 -0700

    Added scrollview element

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react"
+import { StyleSheet, Text, View, ScrollView } from "react-native"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	const [ people, setPeople ] = useState([
+		{ name: "Cam" },
+		{ name: "Athena" },
+		{ name: "Roy" },
+		{ name: "Dempsey" },
+		{ name: "Payton" },
+		{ name: "Cam" },
+		{ name: "Athena" },
+		{ name: "Roy" },
+		{ name: "Dempsey" },
+		{ name: "Payton" }
+	])
+	return (
+		<View style={styles.container}>
+			<ScrollView>
+				{people.map((p, i) => (
+					<View key={i}>
+						<Text style={styles.person}>{p.name}</Text>
+					</View>
+				))}
+			</ScrollView>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		paddingTop: 40,
+		paddingHorizontal: 20,
+	},
+	person: {
+		marginTop: 24,
+		padding: 30,
+		backgroundColor: "lightblue",
+		fontSize: 24
+	}
+})
