0ada56a335e11d44c8c185f5767fed2d86c3ab0b
Cameron Nye <csnye86@gmail.com>
2021-02-09 11:27:29 -0700

    Using flatlist element

--- App.js
+++ App.js
@@ -1,28 +1,33 @@
 import React, { useState } from "react"
-import { StyleSheet, Text, View, ScrollView } from "react-native"
+import { StyleSheet, Text, View, FlatList } from "react-native"
 
 export default function App() {
 	const [ people, setPeople ] = useState([
-		{ name: "Cam" },
-		{ name: "Athena" },
-		{ name: "Roy" },
-		{ name: "Dempsey" },
-		{ name: "Payton" },
-		{ name: "Cam" },
-		{ name: "Athena" },
-		{ name: "Roy" },
-		{ name: "Dempsey" },
-		{ name: "Payton" }
+		{ name: "Cam", id: "1" },
+		{ name: "Athena", id: "2" },
+		{ name: "Roy", id: "3" },
+		{ name: "Dempsey", id: "4" },
+		{ name: "Payton", id: "5" },
+		{ name: "Cam", id: "6" },
+		{ name: "Athena", id: "7" },
+		{ name: "Roy", id: "8" },
+		{ name: "Dempsey", id: "9" },
+		{ name: "Payton", id: "10" }
 	])
 	return (
 		<View style={styles.container}>
-			<ScrollView>
-				{people.map((p, i) => (
-					<View key={i}>
-						<Text style={styles.person}>{p.name}</Text>
-					</View>
-				))}
-			</ScrollView>
+			<FlatList
+				numColumns={2}
+				data={people}
+				renderItem={({ item }) => (
+					<Text
+						style={[styles.person, { marginLeft: item.id % 2 === 0 ? 10 : 0, marginRight: item.id % 2 === 1 ? 10 : 0 }]}
+					>
+						{item.name}
+					</Text>
+				)}
+				keyExtractor={item => item.id} // Only necessary if data does not contain 'key' value
+			/>
 		</View>
 	)
 }
@@ -31,13 +36,15 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: "#fff",
-		paddingTop: 40,
-		paddingHorizontal: 20,
+		paddingTop: 40
 	},
 	person: {
 		marginTop: 24,
 		padding: 30,
 		backgroundColor: "lightblue",
-		fontSize: 24
+		fontSize: 24,
+		width: "50%",
+		marginHorizontal: 10,
+		textAlign: "center"
 	}
 })
