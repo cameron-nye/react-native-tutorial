bec5acc796d421d18aa010bdac76edeca607a3b9
Cameron Nye <csnye86@gmail.com>
2021-02-16 16:08:39 -0700

    Dismisses keyboard on press

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react"
-import { Alert, FlatList, StyleSheet, Text, View } from "react-native"
+import { Alert, FlatList, Keyboard, StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native"
 import AddToDo from "./components/AddToDo"
-import Header from './components/Header'
+import Header from "./components/Header"
 import ToDoItem from "./components/ToDoItem"
-
 
 export default function App() {
 	const [ todos, setTodos ] = useState([
@@ -12,28 +11,33 @@ export default function App() {
 		{ text: "play on the switch", key: "3" }
 	])
 
-  const deleteItem = (key) => {
-    setTodos(prevState => prevState.filter(item => item.key !== key))
-  }
+	const deleteItem = key => {
+		setTodos(prevState => prevState.filter(item => item.key !== key))
+	}
 
-  const addItem = text => {
-    if(text.length < 3) Alert.alert('OOPS!', 'Todos must be at least 3 chars long', [{
-      text: 'Understood',
-      onPress: () => console.log('alert closed')
-    }])
-    setTodos(prevState => [...prevState, {text, key: Math.random().toString()}])
-  }
+	const addItem = text => {
+		if (text.length < 3)
+			Alert.alert("OOPS!", "Todos must be at least 3 chars long", [
+				{
+					text: "Understood",
+					onPress: () => console.log("alert closed")
+				}
+			])
+		setTodos(prevState => [ ...prevState, { text, key: Math.random().toString() } ])
+	}
 
 	return (
-		<View style={styles.container}>
-      <Header />
-			<View style={styles.content}>
-        <AddToDo addItem={addItem}/>
-				<View style={styles.list}>
-					<FlatList data={todos} renderItem={({ item }) => <ToDoItem item={item} deleteItem={deleteItem}/>} />
+		<TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+			<View style={styles.container}>
+				<Header />
+				<View style={styles.content}>
+					<AddToDo addItem={addItem} />
+					<View style={styles.list}>
+						<FlatList data={todos} renderItem={({ item }) => <ToDoItem item={item} deleteItem={deleteItem} />} />
+					</View>
 				</View>
 			</View>
-		</View>
+		</TouchableWithoutFeedback>
 	)
 }
 
