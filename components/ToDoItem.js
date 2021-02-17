d1820f5d1600b4fc1ec96025e500d880d52f133d
Cameron Nye <csnye86@gmail.com>
2021-02-17 14:15:36 -0700

    Added native icons

--- components/ToDoItem.js
+++ components/ToDoItem.js
@@ -1,24 +1,32 @@
-import React from 'react'
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
+import React from "react"
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native"
+import { MaterialIcons } from "@expo/vector-icons"
 
-const ToDoItem = ({item, deleteItem}) => {
-
-  return (
-    <TouchableOpacity style={styles.item} onPress={() => deleteItem(item.key)}>
-      <Text>{item.text}</Text>
-    </TouchableOpacity>
-  )
+const ToDoItem = ({ item, deleteItem }) => {
+	return (
+		<TouchableOpacity style={styles.itemWrapper} onPress={() => deleteItem(item.key)}>
+			<View style={styles.item}>
+        <MaterialIcons name='delete' size={18} color='#333'/>
+				<Text style={styles.itemText}>{item.text}</Text>
+			</View>
+		</TouchableOpacity>
+	)
 }
 
 const styles = StyleSheet.create({
-  item: {
-    padding: 16,
-    marginTop: 16,
-    borderColor: '#bbb',
-    borderWidth: 1,
-    borderStyle: 'solid',
-    borderRadius: 4,
-    backgroundColor: '#fff'
+	item: {
+		padding: 16,
+		marginTop: 16,
+		borderColor: "#bbb",
+		borderWidth: 1,
+		borderStyle: "solid",
+		borderRadius: 4,
+		backgroundColor: "#fff",
+    flexDirection: 'row',
+    alignItems: 'center'
+	},
+  itemText: {
+    marginLeft: 10
   }
 })
 
